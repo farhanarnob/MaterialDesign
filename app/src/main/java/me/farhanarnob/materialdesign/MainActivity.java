16945bfba093b2bcc2f140a3ae3e593c864b8685
Farhan Rahman Arnob <farhanarnob@live.com>
2017-04-06 14:05:58 +0600

    Menu has been implemented.

--- app/src/main/java/me/farhanarnob/materialdesign/MainActivity.java
+++ app/src/main/java/me/farhanarnob/materialdesign/MainActivity.java
@@ -1,7 +1,12 @@
 package me.farhanarnob.materialdesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setBackgroundColor(Color.RED);
+
+        // Adding supportActionBar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //giving menu into the toobar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_for_activity_main_page, menu);
+        return true;
     }
 }
