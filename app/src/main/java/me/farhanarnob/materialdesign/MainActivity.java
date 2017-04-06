10e3332ff64dd44e0d07dd9f88d8d606df40f67c
Farhan Rahman Arnob <farhanarnob@live.com>
2017-04-06 14:16:31 +0600

    Menu item clicking option has been implemented.

--- app/src/main/java/me/farhanarnob/materialdesign/MainActivity.java
+++ app/src/main/java/me/farhanarnob/materialdesign/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    //giving menu into the toobar
+    //giving menu into the toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_for_activity_main_page, menu);
+        return true;
+    }
+
+    // menu item click option has been added here.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about_us:
+                Toast.makeText(MainActivity.this, "You have been clicked about us", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_setting:
+                Toast.makeText(MainActivity.this, "You have been clicked setting", Toast.LENGTH_SHORT).show();
+                break;
+        }
         return true;
     }
 }
